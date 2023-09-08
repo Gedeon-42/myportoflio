29db9b42e8c8adee141880560ed701e1ad688a37
Gedeon-42 <umwizerwagedeon@gmail.com>
2023-09-07 20:47:30 -1200

    changed layout

--- js/script.js
+++ js/script.js
@@ -17,3 +17,28 @@ scrollTop.onclick =function(){
     window.scrollTo(0,0)
     console.log('aded')
 }
+
+// function to validate form
+
+function validateForm(){
+    let validate = document.forms['myForm']['myName'].value
+    if (validate == "") {
+        alert("Name must be filled out");
+        return false;
+      }
+}
+
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "umwizerwagedeon49@gmail.com",
+        Password : "59E54D0C616EA64FC6F0508C8417CFC7D2EF",
+        To : 'umwizerwagedeon@gmail.com',
+        From : document.getElementById('email').value,
+        Subject : "New message from contact",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
